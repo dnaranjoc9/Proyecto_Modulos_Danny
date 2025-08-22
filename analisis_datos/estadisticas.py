232497bb9c50acdcb15d337299aaf7671a0e902e
jjimenezv101 <JJIMENEZV@CTPPLATANARES.ED.CR>
2025-08-22 16:08:35 -0600

    22/08/2025
    Cotidiano

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,1 +1,20 @@
 # estadisticas.py
+def media(datos):
+   media_arimetica = sum(datos) / len(datos)
+   return media_arimetica
+
+def mediana(datos):
+   #mediana
+   #Paso #1 Ordenar
+    datos_ordenados = sorted(datos)
+    n = len(datos_ordenados)
+    mitad = n // 2
+    if n % 2 == 0:
+        mediana = (datos_ordenados[mitad - 1] + datos_ordenados[mitad]) / 2 
+    else:
+        mediana = datos_ordenados[mitad]
+    return mediana
+
+if __name__ == "__main__":
+   notas = [100,50,100,78,100]
+   print(media(notas))
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
+# Proyecto: [Modulos y funciones Python]
+# Estudiante: [Danny Josué Naranjo Campos]
+# Fecha de Inicio: [22/08/2025]
+# Fecha de Entrega: [28/10/2025]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+#from analisis_datos.carga_datos import generar_lista_compras
+#from analisis_datos.estadisticas import media
+
+from analisis_datos import *
+
+def saludar():
+    print("¡Hola! desde la Función")
+    
+if __name__ == "__main__":
+    compras = generar_lista_compras()
+    print(compras)
+    media = media(list(compras.values()))
+    print("Promedio de precios por articulo es: ", media)
+    
+  mediana = mediana(list(compras.values()))
+    print("Mediana de precios por articulo es: ", mediana)
+    
